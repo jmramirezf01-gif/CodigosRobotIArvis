75a40e4f741da7f5a6e9cfdd7ebc069512bc1a04
jmramirezf01-gif <jmramirezf01@liverpool.com.mx>
2025-12-19 12:52:03 -0600

    Update pruebas_step_motor.ino

--- pruebas_step_motor/pruebas_step_motor.ino
+++ pruebas_step_motor/pruebas_step_motor.ino
@@ -1,4 +1,4 @@
-// Control de un motor a pasos con driver DRV8825
+// Control de un motor a pasos con driver DRV8825 y arduino MEGA 
 #include "AccelStepper.h"
 
 // Definiendo las conexiones del motor 
@@ -25,39 +25,44 @@ AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 //AccelStepper stepper2 = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 
-bool moving = true;
+//bool moving = true;
 
 
 void setup() {
 
+  /*En esta seccion se establece el setpoint de cada carro del riel*/
   stepper.setMaxSpeed(1000);
   stepper.setAcceleration(500);
   stepper.setSpeed(500);
   //stepper2.setMaxSpeed(500);
 
 
+  // Seccion donde se define al pin de datos del limite de carrera como INPUT
   pinMode(limitpin, INPUT);
   val = digitalRead(limitpin); 
+
+  // El ciclo while permite que el carrete se desplace mientras no se presione 
+  // el boton del final de carrera
   while (val == HIGH){
     stepper.setSpeed(-500);
     stepper.runSpeed();
     val = digitalRead(limitpin);
   }
 
+  //
   stepper.setCurrentPosition(0);
+  Serial.print("\nReferencia encontrada");
   stepper.runToNewPosition(4000);
   stepper.setCurrentPosition(0);
-
-
-
-
+  stepper.stop();
+  delay(4000);
 
 }
 /*  Esta seccion se encarga de mover el motor hasta cierta posicion 
     una vez que se establece el setpoint*/
 void loop() {
 
-  stepper.moveTo(-400);
+  stepper.moveTo(-4000);
   while(stepper.distanceToGo() != 0){
     stepper.run();
   }
